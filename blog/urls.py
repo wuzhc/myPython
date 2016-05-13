8616e59b0a691d65bdd47d4c8ae5a6fc95f14388
wuzhc <wzc1716220125@163.com>
2016-05-14 00:06:15 +0800

    添加blog模块

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import patterns,include,url
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.post_list, name="post_list"),
+    #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"), #http://127.0.0.1:8000/post/1/
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
 ]
+
+# urlpatterns = patterns('',
+#     url(r'^$', views.post_list, name="post_list"),
+#     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
+#     # url(r'^$', 'mysite.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+# )
