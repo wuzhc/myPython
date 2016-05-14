d24bfda49ec3ac65851c676cab2ce461cf37ab37
wuzhc <wzc1716220125@163.com>
2016-05-14 11:42:31 +0800

    完善blog模块

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     url(r'^$', views.post_list, name="post_list"),
     #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"), #http://127.0.0.1:8000/post/1/
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
+    url(r'^post/new/$', views.post_new, name="post_new"),
+    url(r'^post/draft/$', views.post_draft_list, name="post_draft_list"),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
 ]
 
 # urlpatterns = patterns('',
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,59 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from .models import Post
+from .forms import PostForm
 
+#博客列表
 def post_list(request):
-    posts = Post.objects.filter(published_date__isnull = False)
+    posts = Post.objects.filter(published_date__isnull = False).order_by("-create_date")
+    #posts = Post.objects.order_by("-create_date")
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+#博客详情
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+
+#创建博客
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False) #参数commit，赋值为False，代表不要提交到数据库
+            post.author = request.user
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_new.html', {'form':form})
+
+#编辑博客
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_new.html', {'form':form})
+
+#发布博客
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog:post_detail', pk=post.pk)
+
+#未发布博客列表
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull = True).order_by("-create_date")
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+#删除博客
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog:post_list')
